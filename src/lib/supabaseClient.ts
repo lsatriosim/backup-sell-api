41335c451069fa30759e7e818f360c239fff5c3b
Liefran Satrio Sim <Liefran123@gmail.com>
2025-09-25 21:46:51 +0700

    fix: change service key to anon key

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -6,7 +6,7 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.SUPABASE_ANON_KEY!;
 const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseServiceKey, {
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
     autoRefreshToken: false,
     persistSession: false
